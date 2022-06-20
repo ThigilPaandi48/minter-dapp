343f0286c3beefda4decdf0b674cb39a3652307a
LAKSHMAN SRINIVASAN <lakshmanhussey@gmail.com>
2022-06-20 10:27:48 +0200

    rinkebytest

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x732158C50f565ac098d51E5d65Ae17917Ac44e14"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
